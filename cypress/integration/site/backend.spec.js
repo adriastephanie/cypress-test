12ab549abe985cc343a8f6b4aa5d4a11fb6b72bd
Adria Stephanie <adriastephanie@hotmail.com>
2021-05-16 21:19:53 -0300

    feature: back - test check response create account

--- cypress/integration/site/backend.spec.js
+++ cypress/integration/site/backend.spec.js
@@ -16,6 +16,22 @@ describe('Should test a functional level', () => {
                 redirecionar: false
             }
         }).its('body.token').should('not.be.empty')
+            .then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}` },
+                body: {
+                    nome: 'Conta via request'
+                }
+            }).as('response')
+        })
+
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via request')
+        })
     })
 
     it('update an account', () => {
