44bf5ad7d58b9a0f88b83e10b68954955db029dc
Adria Stephanie <adriastephanie@hotmail.com>
2021-06-07 19:55:58 -0300

    chore: modification tests

--- cypress/integration/site/backend.spec.js
+++ cypress/integration/site/backend.spec.js
@@ -19,7 +19,7 @@ describe('Should test a BACKEND level', () => {
 
     it('Should create an account', () => {
             cy.request({
-                url: 'https://barrigarest.wcaquino.me/contas',
+                url: '/contas',
                 method: 'POST',
                 headers: { Authorization: `JWT ${token}` },
                 body: {
@@ -33,44 +33,8 @@ describe('Should test a BACKEND level', () => {
             expect(res.body).to.have.property('nome', 'Conta via request')
         })
     })
-})
 
-
-/* import '../../support/commands'
-
-describe('Should test a BACKEND level', () => {
-
-    let token
-
-    before(() => {
-        cy.getToken('adria@teste.com', '123')
-        .then(tkn => {
-            token = tkn
-        })
-      
-    })
-    beforeEach(() => {
-        cy.resetRest()
-    })
-    
-    it('Should create an account', () => {
-        cy.request({
-            url: 'https://barrigarest.wcaquino.me/contas',
-            method: 'POST',
-            headers: { Authorization: `JWT ${token}` },
-            body: {
-                nome: 'Conta via request'
-            }
-        }).as('response')
-
-    cy.get('@response').then(res => {
-        expect(res.status).to.be.equal(201)
-        expect(res.body).to.have.property('id')
-        expect(res.body).to.have.property('nome', 'Conta via request')
-    })
-})
-
-     it('update an account', () => {
+    it('update an account', () => {
         cy.request({
             method:'GET',
             url: '/contas',
@@ -142,6 +106,6 @@ describe('Should test a BACKEND level', () => {
 
     it('test remove a transaction', () => {
 
-    })
+    })  
 
-}) */
+})
