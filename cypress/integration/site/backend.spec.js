8da82f29d8c097469412d5d7bb49bbbd88e41aad
Adria Stephanie <adriastephanie@hotmail.com>
2021-06-07 20:24:03 -0300

    config

--- cypress/integration/site/backend.spec.js
+++ cypress/integration/site/backend.spec.js
@@ -18,21 +18,21 @@ describe('Should test a BACKEND level', () => {
     })
 
     it('Should create an account', () => {
-            cy.request({
-                url: '/contas',
-                method: 'POST',
-                headers: { Authorization: `JWT ${token}` },
-                body: {
-                    nome: 'Conta via request'
-                }
-            }).as('response')
+        cy.request({
+            url: 'https://barrigarest.wcaquino.me/contas',
+            method: 'POST',
+            headers: { Authorization: `JWT ${token}` },
+            body: {
+                nome: 'Conta via request'
+            }
+        }).as('response')
 
-        cy.get('@response').then(res => {
-            expect(res.status).to.be.equal(201)
-            expect(res.body).to.have.property('id')
-            expect(res.body).to.have.property('nome', 'Conta via request')
-        })
+    cy.get('@response').then(res => {
+        expect(res.status).to.be.equal(201)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('nome', 'Conta via request')
     })
+})
 
     it('update an account', () => {
         cy.request({
